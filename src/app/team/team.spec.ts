ad49b626b8caa762ad218f7ead35202ef62a23c5
Daniela Alvarado <d@ignisaltus.com>
2019-02-12 15:25:05 -0600

    added test for team

---
+++ src/app/team/team.spec.ts
@@ -0,0 +1,14 @@
+import {team} from './team';
+
+describe('team', () =>{
+    var input1 = '3\n1 1 0\n1 1 1\n1 0 0';
+    var input2 = '2\n1 0 0\n0 1 1';
+    it('should return 2 if the input is input1', () => {
+        const result = team(input1);
+        expect(result).toBe(2);
+    })
+    it('should return 1 if the input is input2', () => {
+        const result = team(input1);
+        expect(result).toBe(1);
+    })
+})
