79a94ad2beb85145399e59fcec7cf87cb263bbf8
BenderCharls <carloscuellarbrito.94@hotmail.com>
2019-02-18 11:45:03 -0600

    Solved stones problem with its tests

--- src/app/codigo1/petya1.ts
+++
@@ -1,9 +0,0 @@
-export function petya1(string1, string2){
-
-	var cadenaUno = string1.toLowerCase();
-	var cadenaDos = string2.toLowerCase();
-	var compara = cadenaUno.localeCompare(cadenaDos);
-
-	return compara;
-		
-}
